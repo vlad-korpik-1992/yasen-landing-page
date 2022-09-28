ba283ee846eb461ce664517ccaa87eccd9a2aad1
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-09-28 15:26:38 +0300

    project completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
+    $('.partners__slider').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		rows: 1,
+		dots: false,
+		autoplay: true,
+		infinite: true,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 1199,
+				settings: {
+				  slidesToShow: 4
+				}
+			  },
+           {
+             breakpoint: 992,
+             settings: {
+               slidesToShow: 3
+             }
+           },
+		   {
+			breakpoint: 500,
+			settings: {
+			  slidesToShow: 2
+			}
+		  },
+		  {
+			breakpoint: 424,
+			settings: {
+			  slidesToShow: 1
+			}
+		  }
+       ] 
+	});
     $('.about__box__title').click(function (e) {
         e.preventDefault();
         let elem = e.target;
@@ -9,4 +44,24 @@ $(document).ready(function() {
         let idElem = elem.getAttribute('id');
 		jQuery("#"+idElem).addClass('about__box__title--active');
 	});
+	$('.menu__btn').on('click', function (e) {
+        e.preventDefault();
+        $('.menu__btn').toggleClass('menu__btn--close');
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.menu__btn-element').toggleClass('menu__btn-element_close');
+    });
+});
+/* scrollto */
+
+$('.scrollto a').on('click', function scroll(e) {
+    e.preventDefault();
+    let href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(href).offset().top - 30
+    }, {
+        duration: 370,
+        easing: "linear"
+    });
+    $('.menu__list').removeClass('menu__list--active');
+    return false;
 });
